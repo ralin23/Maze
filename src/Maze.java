6af274cb7ec6fe9415bc8a8f61c31c5256f2ad03
Raymond Lin <21212089+ralin23@users.noreply.github.com>
2019-11-09 02:49:35 -0800

    Created initial comments for future coding to be based on.
    Signed-off-by: Raymond Lin <21212089+ralin23@users.noreply.github.com>

--- src/Maze.java
+++ src/Maze.java
@@ -1,6 +1,15 @@
 import java.util.Stack;
 
 public class Maze {
+    /* Maze Representation 3x3 Ex:
+    + +----     where 0 is entry and 8 is exit
+    |0|1|2|     Using an array of adjacency lists, each number is an index of an array A
+    + +----     A[index] has a linked list which contains connected paths to that node
+    |3 4|5|     Example to left shows A[0] -> 3, A[3] -> 4
+    -------     Legend: + corners, | or - walls
+    |6|7|8|
+    ----+ +
+    */
     private MazeCell[][] maze;
     private int row;
     private int column;
